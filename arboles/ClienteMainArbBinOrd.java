ac9b0960f9a51f17417d4aa18e83473dcccfc2ba
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-11-25 15:02:05 +0000

    terminamos uso de arbol binario ordenado

--- arboles/ClienteMainArbBinOrd.java
+++ arboles/ClienteMainArbBinOrd.java
@@ -11,20 +11,61 @@ public class ClienteMainArbBinOrd {
              ab.insertar(e1);
              ab.insertar(e2);
              ab.insertar(e3);
+             ab.insertar2(e4); //version recursiva
              ab.insertar(e4);
-             System.out.println("\nRecorrido PreOrden");
-             ab.preorden();
-             System.out.println("\nRecorrido InOrden");
-             ab.inorden();
-             System.out.println("\nRecorrido PostOrden");
-             ab.postorden();
-             System.out.println();
-             System.out.println("Arbol Jerarquico");
-             BTreePrinter.printNode(ab.getRaiz());
+
+            
 
         }catch(Exception ex){
             System.out.println("Error: ocurrio una excepcion"+ex.getMessage());
         }
+            System.out.println("\nRecorrido PreOrden");
+            ab.preorden();
+            System.out.println("\nRecorrido InOrden");
+            ab.inorden();
+            System.out.println("\nRecorrido PostOrden");
+            ab.postorden();
+            System.out.println();
+            System.out.println("Arbol Jerarquico");
+            BTreePrinter.printNode(ab.getRaiz());
+            //busqueda
+
+            //busqueda iterativa
+            Nodo aux = ab.buscar(e4);
+            if(aux ==null){
+                System.out.println("\nDato no encontrado");
+            }else{
+                System.out.println("\nEncontrado el dato: "+aux.getValor());
+            }
+
+            //busqueda recursiva
+            Estudiante e5 = new Estudiante(124,"Daniel Parra");
+            aux = ab.buscar2(e5);
+            if(aux ==null){
+                System.out.println("\nDato no encontrado");
+            }else{
+                System.out.println("\nEncontrado el dato: "+aux.getValor());
+            }
+            try{
+                ab.eliminar(e3);
+                System.out.println();
+                System.out.println("Arbol Jerarquico despues de eliminar e3:");
+                BTreePrinter.printNode(ab.getRaiz());
+                ab.eliminar(e1);
+                System.out.println("\nArbol Jerarquico despues de eliminar e1:");
+                BTreePrinter.printNode(ab.getRaiz());
+                ab.eliminar(e1);
+
+
+
+            }catch(Exception ex){
+                System.out.println("Error: ocurrio una excepcion"+ex.getMessage());
+            }
+
+            
+
+
+
 
     }
     
